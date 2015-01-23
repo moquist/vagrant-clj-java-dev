2ea96d1965b1c0fa75db018849cc35334d753d23
Matt Oquist <moquist@majen.net>
2015-01-23 14:06:51 -0500

    Note that copying to ~/Desktop should be last.
    This makes it easy for a user who has signed in to the guest GUI to know the
    provisioning is complete.

--- user-config.sh
+++ user-config.sh
@@ -4,4 +4,7 @@
 git clone https://github.com/moquist/dotfiles.pub
 mv ~/dotfiles.pub ~/.dotfiles.pub
 bash ~/.dotfiles.pub/bin.pub/dotfiles.pub-set-up
+
+# Keep this step last, so the user in the guest GUI has a clear indication when
+# provisioning is complete.
 rsync -av /vagrant/desktop-contents/ ~vagrant/Desktop/
