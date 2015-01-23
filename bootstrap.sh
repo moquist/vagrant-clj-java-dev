36460e397f79a412c52ccccf260a3c2a15870be9
Matt Oquist <moquist@majen.net>
2015-01-23 17:03:11 -0500

    Convert user-config.sh to UNIX format. (ugh)
    bootstrap.sh works on Windows host system even when it's in DOS format.
    user-config.sh, which is launched by bootstrap.sh, does *not* work when it's in
    DOS format. So we must convert it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,5 +10,7 @@ apt-get install -y tmux
 apt-get install -y vim emacs24
 apt-get install -y eclipse eclipse-egit eclipse-subclipse eclipse-subclipse-graph eclipse-wtp eclipse-wtp-servertools eclipse-wtp-webtools eclipse-wtp-ws eclipse-wtp-xmltools eclipse-wtp-xsl
 apt-get install -y git
+apt-get install -y dos2unix # Ugh. Yeah, you heard me.
+/usr/bin/dos2unix /vagrant/user-config.sh # Yeah. Seriously.
 su - -c "bash /vagrant/user-config.sh" vagrant
 
